2f4974d7a479ec834d918972dd046ef55cbae3c6
RobertHJones <roberthuwjones@outlook.com>
2021-12-22 16:46:51 +0000

    kata-written

--- main.test.js
+++ main.test.js
@@ -14,7 +14,9 @@ import uncorruptTheString from "./main.js";
 // __________________________
 
 const testArray = [
-    ["t5h3e", "the"]
+    ["h156t75e1et y^^56n12a!!!!g5o6h99999a1m 15d::n19791979e-----i1313rf89lr1ig y0000m", "my girlfriend has mahogany teeth"], 
+    ["n457ee+=+=rg t000000ser^^^^^^^^up!!!! f88888*o t90909egg!un><><><>< a\ d)(na???h latr33333333\|/om y0m n;i er_-eh d###lo!h .I. e*b t#i dl09090909u123456123oc", "could it be I hold here in my mortal hand a nugget of purest green"],
+    []
 ]
 
 test.each(testArray)(`should read "%s" as "%s"`, function(corruptedString, correctString) {
